233f1c44f05e63ae2ca6bc0edc3c74bd61e238b1
lizekai-richard <lzknus@gmail.com>
2024-09-10 13:04:26 +0800

    Try different memory strides

--- perf.py
+++ perf.py
@@ -1,15 +1,49 @@
 import os
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from sam2.build_sam import build_sam2_video_predictor
+from PIL import Image
 
 checkpoint = "./checkpoints/sam2_hiera_base_plus.pt"
 model_cfg = "sam2_hiera_b+.yaml"
 predictor = build_sam2_video_predictor(model_cfg, checkpoint)
 
+video_dir = "./notebooks/videos/bedroom"
+frame_names = [
+    p for p in os.listdir(video_dir)
+    if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+]
+frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
+
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=200):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+
 
 with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
-    state = predictor.init_state("./video_samples/")
+    state = predictor.init_state(video_dir)
 
     # add new prompts and instantly get the output on the same frame
     ann_frame_idx = 0
@@ -34,3 +68,12 @@ with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
             out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
             for i, out_obj_id in enumerate(out_obj_ids)
         }
+
+    vis_frame_stride = 10
+    plt.close("all")
+    for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
+        plt.figure(figsize=(6, 4))
+        plt.title(f"frame {out_frame_idx}")
+        plt.imshow(Image.open(os.path.join(video_dir, frame_names[out_frame_idx])))
+        for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+            show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
